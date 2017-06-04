405e54a28446df5af973574be947ee5ea2799275
Giovanni Bieller <giovanni.bieller@gmail.com>
2017-06-05 00:25:00 +0200

    adds

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -13,6 +13,7 @@ import browserSync from 'browser-sync';
 const paths = {
     root: './',
     scss: './scss',
+    demo: './demo',
     dist: './dist'
 };
 
@@ -34,6 +35,29 @@ gulp.task('copy-html', () => {
     return gulp
         .src([paths.root + 'index.html'])
         .pipe(gulp.dest(paths.dist))
+        .pipe(browserSync.reload({ stream: true }));
+});
+
+/**
+ * Copy demo
+ */
+gulp.task('copy-demo', () => {
+    return gulp
+        .src([paths.demo + '/demo.css'])
+        .pipe(
+            sass({ outputStyle: 'compressed' }).on(
+                'error',
+                notify.onError(error => {
+                    return 'Error: ' + error.message;
+                })
+            )
+        )
+        .pipe(
+            rename({
+                suffix: '.min'
+            })
+        )
+        .pipe(gulp.dest(paths.dist + '/css'))
         .pipe(browserSync.reload({ stream: true }));
 });
 
@@ -76,11 +100,12 @@ gulp.task('default', ['dist']);
 gulp.task('watch', ['dist', 'browser-sync'], () => {
     gulp.watch([paths.scss + '/*.scss'], ['scss']);
     gulp.watch([paths.root + 'index.html'], ['copy-html']);
+    gulp.watch([paths.demo + '/demo.css'], ['copy-demo']);
 });
 
 /**
  * Dist
  */
 gulp.task('dist', ['clean-dist-css'], cb => {
-    runSequence('copy-html', 'scss', cb);
+    runSequence('copy-demo', 'copy-html', 'scss', cb);
 });
